747bf9d91b0bccddb0108ddcb9e518a37ccf83ad
Guillaume Sallé <gusalle@student.42.fr>
2023-07-11 15:09:46 +0200

    add test echo -n

--- tests/test_echo.c
+++ tests/test_echo.c
@@ -21,6 +21,27 @@ START_TEST(test_echo)
 }
 END_TEST
 
+START_TEST(test_echo)
+{
+	char* test_strings[] = {"echo", "-n", "Hello,", "World!", NULL};
+
+	if (redirect_stdout_to_buffer() == -1) {
+        ck_abort_msg("Failed to redirect stdout to buffer");
+    }
+
+    echo(test_strings);
+
+    char buffer[128];
+    ssize_t len = restore_stdout_and_read_buffer(buffer, sizeof(buffer));
+    if (len == -1) {
+        ck_abort_msg("Failed to restore stdout and read buffer");
+    }
+
+	char* expected_output = "Hello, World!";
+    ck_assert_msg(strcmp(buffer, expected_output) == 0, "Output was '%s'", buffer);
+}
+END_TEST
+
 Suite* echo_suite() {
     Suite* s = suite_create("echo");
     TCase* tc_core = tcase_create("Core");
