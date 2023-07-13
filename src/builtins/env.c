cbb8012b8cb94d7481deab91d4e6ecc602ffbc60
Guillaume Sallé <gusalle@student.42.fr>
2023-07-13 18:25:57 +0200

    update env.c

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: gusalle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 20:01:29 by gusalle           #+#    #+#             */
-/*   Updated: 2023/07/11 20:24:32 by gusalle          ###   ########.fr       */
+/*   Updated: 2023/07/13 18:25:43 by gusalle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	env(t_list2 *head)
 		ft_putstr_fd(current->name, 1);
 		ft_putstr_fd("=", 1);
 		ft_putstr_fd(current->content, 1);
-		ft_putstr_fd("\n");
+		ft_putstr_fd("\n", 1);
 		current = current->next;
 	}
 }
