5f92fc64ab8437f14664a23ae3ee5b3da826d285
Guillaume Sallé <gusalle@student.42.fr>
2023-07-13 17:44:08 +0200

    fix minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: gusalle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/09 17:33:58 by gusalle           #+#    #+#             */
-/*   Updated: 2023/07/11 20:23:25 by gusalle          ###   ########.fr       */
+/*   Updated: 2023/07/13 17:43:07 by gusalle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@
 # define ECHO_OPTIONS "n"
 # define OPTIONS_SIZE 128
 
-typedef struct s_list
+typedef struct s_list2
 {
 	char			*name;
 	char			*content;
-	struct s_list	*next;
+	struct s_list2	*next;
 }	t_list2;
 
 void	echo(int argc, char **argv);
