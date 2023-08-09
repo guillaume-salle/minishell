bcb8f137d0d37a8a471a4c45522f402ad329f05b
Guillaume SALLE <guillaume.salle@gmail.com>
2023-08-09 16:16:04 +0200

    cd update PWD and OLDPWd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: gusalle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 18:43:12 by gusalle           #+#    #+#             */
-/*   Updated: 2023/07/17 21:44:54 by gusalle          ###   ########.fr       */
+/*   Updated: 2023/08/09 16:14:47 by gusalle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,19 @@ extern t_global	g_vars;
 
 int	cd(int argc, char *argv[])
 {
-	char	*path;
-
 	if (argc > 2)
 	{
 		ft_putstr_fd("cd: too many arguments\n", STDERR_FILENO);
 		return (-1);
 	}
-	else
-		path = argv[1];
-	if (chdir(path) != 0)
+	else if (argc < 2)
+		return (-1);
+	if (chdir(argv[1]) != 0)
 	{
 		perror("cd");
 		return (-1);
 	}
+	my_putenv("PWD", getenv("PWD"));
+	my_putenv("OLDPWD", getenv("OLDPWD"));
 	return (0);
 }
