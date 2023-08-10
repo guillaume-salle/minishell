4eac964ab2131f809aa32323c278fec59a69e091
Guillaume SALLE <guillaume.salle@gmail.com>
2023-08-10 18:57:43 +0200

    env working, cd WIP

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gusalle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 14:43:32 by gusalle           #+#    #+#             */
-/*   Updated: 2023/07/17 19:56:31 by gusalle          ###   ########.fr       */
+/*   Updated: 2023/08/10 18:55:15 by gusalle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,8 @@ int	main(int argc, char *argv[], char *envp[])
 	argc++;
 	(void) argv;
 	g_vars.envp_list = init_envp_list(envp);
+	if (!g_vars.envp_list)
+		return (-1);
+	env(g_vars.envp_list);
 	return (0);
 }
