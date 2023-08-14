fb0873727538d4359aff965958e1865c586e209c
Guillaume SALLE <guillaume.salle@gmail.com>
2023-08-14 11:56:36 +0200

    norme

--- src/builtins.c
+++ src/builtins.c
@@ -6,7 +6,7 @@
 /*   By: gusalle <gusalle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/12 19:54:05 by gusalle           #+#    #+#             */
-/*   Updated: 2023/08/12 20:52:23 by gusalle          ###   ########.fr       */
+/*   Updated: 2023/08/14 11:55:47 by gusalle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,19 @@ int	exec_builtins(int argc, char *argv[], t_vars *vars)
 	char	*cmd_name;
 
 	cmd_name = argv[0];
-	if (ft_cmd_namecmp(cmd_name, "echo") == 0)
+	if (ft_strcmp(cmd_name, "echo") == 0)
 		ret = echo(argc, argv, vars);
-	if (ft_cmd_namecmp(cmd_name, "cd") == 0)
+	if (ft_strcmp(cmd_name, "cd") == 0)
 		ret = cd(argc, argv, vars);
-	if (ft_cmd_namecmp(cmd_name, "pwd") == 0)
+	if (ft_strcmp(cmd_name, "pwd") == 0)
 		ret = pwd(argc, argv, vars);
-	if (ft_cmd_namecmp(cmd_name, "export") == 0)
+	if (ft_strcmp(cmd_name, "export") == 0)
 		ret = export(argc, argv, vars);
-	if (ft_cmd_namecmp(cmd_name, "unset") == 0)
+	if (ft_strcmp(cmd_name, "unset") == 0)
 		ret = unset(argc, argv, vars);
-	if (ft_cmd_namecmp(cmd_name, "env") == 0)
+	if (ft_strcmp(cmd_name, "env") == 0)
 		ret = env(argc, argv, vars);
-	if (ft_cmd_namecmp(cmd_name, "exit") == 0)
+	if (ft_strcmp(cmd_name, "exit") == 0)
 		ret = exit(argc, argv, vars);
 	return (ret);
 }
