fecc40f675ecc585c817915645bf4dbfdb53fb8e
Guillaume Sallé <gusalle@student.42.fr>
2023-09-14 12:30:13 +0200

    solve empty command

--- src/utils_exec/handle_history.c
+++ src/utils_exec/handle_history.c
@@ -6,14 +6,19 @@
 /*   By: gusalle <gusalle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 21:00:16 by gusalle           #+#    #+#             */
-/*   Updated: 2023/09/06 15:49:57 by gusalle          ###   ########.fr       */
+/*   Updated: 2023/09/14 11:55:44 by gusalle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell_exec.h"
 
-void	handle_history(char *line)
+bool	handle_history(char *line)
 {
 	if (!ft_str_isspace(line))
+	{
 		add_history(line);
+		return (true);
+	}
+	else
+		return (false);
 }
