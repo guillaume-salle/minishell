d45fb8f61e9e80a30672bc04e489ccd2eb28cde6
Guillaume SALLE <guillaume.salle@gmail.com>
2023-09-15 18:39:14 +0200

    refactor export

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,22 +6,28 @@
 /*   By: gusalle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 20:01:29 by gusalle           #+#    #+#             */
-/*   Updated: 2023/09/06 15:48:59 by gusalle          ###   ########.fr       */
+/*   Updated: 2023/09/15 18:09:41 by gusalle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell_exec.h"
 
-void	print_env(t_list *head)
+void	print_env(const char *prefix, t_list *head)
 {
 	t_list	*current;
 
 	current = head;
 	while (current != NULL)
 	{
+		if (prefix != NULL)
+			ft_putstr_fd(prefix, STDOUT_FILENO);
 		ft_putstr_fd(current->name, STDOUT_FILENO);
 		ft_putstr_fd("=", STDOUT_FILENO);
+		if (prefix != NULL)
+			ft_putstr_fd("\"", STDOUT_FILENO);
 		ft_putstr_fd(current->content, STDOUT_FILENO);
+		if (prefix != NULL)
+			ft_putstr_fd("\"", STDOUT_FILENO);
 		ft_putstr_fd("\n", STDOUT_FILENO);
 		current = current->next;
 	}
@@ -31,6 +37,6 @@ int	env(int argc, char *argv[], t_vars *vars)
 {
 	(void) argc;
 	(void) argv;
-	print_env(vars->envp_list);
+	print_env(NULL, vars->envp_list);
 	return (0);
 }
