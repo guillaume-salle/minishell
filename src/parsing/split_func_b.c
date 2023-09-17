810ccd11ba54ee9c79086d1d5003d3c20e723244
Kenzy YACINI <kyacini@made-f0Cr7s5.clusters.42paris.fr>
2023-09-17 16:08:51 +0200

    heredoc var and segfault >

--- src/parsing/split_func_b.c
+++ src/parsing/split_func_b.c
@@ -6,13 +6,13 @@
 /*   By: kyacini <kyacini@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 14:26:10 by kyacini           #+#    #+#             */
-/*   Updated: 2023/09/16 16:35:02 by gusalle          ###   ########.fr       */
+/*   Updated: 2023/09/17 14:07:13 by kyacini          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell_parsing.h"
 
-void	fill_lstcmd(char **div, int *tab, t_commande *c, t_vars *var_env)
+void	fill_lstcmd(char **div, int *tab, t_commande **c, t_vars *var_env)
 {
 	int		i;
 	char	*without_exp;
@@ -23,13 +23,13 @@ void	fill_lstcmd(char **div, int *tab, t_commande *c, t_vars *var_env)
 		if (tab[i] != 0)
 			without_exp = ft_strdup3(div[i]);
 		if (tab[i] == 1)
-			ft_lstadd_backcmd(&c, ft_lst_newcmd(without_exp, supp_quotes(div[i], var_env), 1));
+			ft_lstadd_backcmd(c, ft_lst_newcmd(without_exp, supp_quotes(div[i], var_env, tab[i]), 1));
 		else if (tab[i] == 2)
-			ft_lstadd_backcmd(&c, ft_lst_newcmd(without_exp, supp_quotes(div[i], var_env), 2));
+			ft_lstadd_backcmd(c, ft_lst_newcmd(without_exp, supp_quotes(div[i], var_env, tab[i]), 2));
 		else if (tab[i] == 3)
-			ft_lstadd_backcmd(&c, ft_lst_newcmd(without_exp, supp_quotes(div[i], var_env), 3));
+			ft_lstadd_backcmd(c, ft_lst_newcmd(without_exp, supp_quotes(div[i], var_env, tab[i]), 3));
 		else if (tab[i] == 4)
-			ft_lstadd_backcmd(&c, ft_lst_newcmd(without_exp, supp_quotes(div[i], var_env), 4));
+			ft_lstadd_backcmd(c, ft_lst_newcmd(without_exp, supp_quotes(div[i], var_env, tab[i]), 4));
 		else if (tab[i] == 5)
 		{
 			free(without_exp);
