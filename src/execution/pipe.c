4e65e04d6016617d0b284e97954cefa8ca7c7ad0
Guillaume SALLE <guillaume.salle@gmail.com>
2023-09-23 20:43:22 +0200

    missing head->next

--- src/execution/pipe.c
+++ src/execution/pipe.c
@@ -6,7 +6,7 @@
 /*   By: gusalle <gusalle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 20:10:16 by gusalle           #+#    #+#             */
-/*   Updated: 2023/09/23 20:18:24 by gusalle          ###   ########.fr       */
+/*   Updated: 2023/09/23 20:43:10 by gusalle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,8 +87,10 @@ void	exec_partition_with_pipe(t_partition *head, t_vars *vars)
 	safe_pipe(first_pipe, vars);
 	exec_first_partition_pipe(head, first_pipe[1],  vars);
 	safe_close(first_pipe[1], vars);
+	head = head->next;
 	while (head != NULL && head->next != NULL)
 	{
+		printf("in here \n");
 		safe_pipe(second_pipe, vars);
 		exec_middle_partition_pipe(head, first_pipe[0], second_pipe[1], vars);
 		safe_close(first_pipe[0], vars);
