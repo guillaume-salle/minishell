f6ad479b833bbc0846c62d0a2d72b3b470cdb63e
Guillaume Sallé <gusalle@student.42.fr>
2023-09-28 17:28:20 +0200

    ctrl c return value

--- src/execution/wait_for_children.c
+++ src/execution/wait_for_children.c
@@ -6,30 +6,33 @@
 /*   By: gusalle <gusalle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 12:56:31 by gusalle           #+#    #+#             */
-/*   Updated: 2023/09/27 20:27:32 by gusalle          ###   ########.fr       */
+/*   Updated: 2023/09/28 17:26:52 by gusalle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell_exec.h"
 
-static bool	case_terminated_by_signal(int status, pid_t wait_pid, t_vars *vars)
+static void	case_terminated_by_signal(int status, pid_t wait_pid,
+		bool *newline,t_vars *vars)
 {
 	int		signal_number;
 
 	signal_number = WTERMSIG(status);
-	if (signal_number == SIGINT)
-		return (true);
 	if (wait_pid == vars->last_pid)
 	{
 		vars->last_exit_status = 128 + signal_number;
 		if (signal_number == SIGINT)
-			return (true);
+		{
+			*newline = true;
+			return ;
+		}
 		if (__WCOREDUMP(status))
 			ft_putstr_fd("Quit (core dumped)\n", STDERR_FILENO);
 		else
 			ft_putstr_fd("Terminated\n", STDERR_FILENO);
 	}
-	return (false);
+	if (signal_number == SIGINT)
+		*newline = true;
 }
 
 void	wait_for_children_2(pid_t wait_pid, bool newline, t_vars *vars)
@@ -53,10 +56,7 @@ void	wait_for_children(t_vars *vars)
 		if (WIFEXITED(status) && wait_pid > 0 && wait_pid == vars->last_pid)
 			vars->last_exit_status = WEXITSTATUS(status);
 		else if (WIFSIGNALED(status))
-		{
-			if (newline == false)
-				newline = case_terminated_by_signal(status, wait_pid, vars);
-		}
+			case_terminated_by_signal(status, wait_pid, &newline, vars);
 		wait_pid = wait(&status);
 		if (wait_pid > 0)
 			newline = false;
